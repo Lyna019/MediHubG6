659464365f1555ffc093c74bf1ab6e7138a54a34
Lyna.Ikhelef <125125447+Lyna019@users.noreply.github.com>
2024-01-27 22:32:43 +0100

    Delete lib directory

--- lib/commons/sentences.dart
+++
@@ -1,8 +0,0 @@
-
-const firstCategory = "Mobility Aids";
-const secondCategory = "Diagnostic Equipments";
-const thirdCategory = "Medical Supplies";
-const fourthCategory = "Medical Equipements";
-
-
-/////////////////////////////////////////Product Detail
